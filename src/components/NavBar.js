4350c969c50af0ec24dd1e19b54462eebd55b89b
Mahe Chen <111488731+lmposter@users.noreply.github.com>
2023-08-09 22:44:58 -0400

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -125,7 +125,7 @@ const NavBar = () => {
 
           <button
             onClick={() => setMode(mode === "light" ? "dark" : "light")}
-            className={`ml-3 flex items-center justify-center rounded-full p-1 ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}
+            className={`w-6 h-6 ml-3 flex items-center justify-center rounded-full p-1 ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}
      `}
           >
             {

ce874106d1bce24a29f207f4e44fba9936a9e6b4
Mahe Chen <111488731+lmposter@users.noreply.github.com>
2023-08-09 20:38:45 -0400

    Update HireMe.js

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -11,7 +11,7 @@ const HireMe = () => {
       <div className="w-48 h-auto flex items-center justify-center relative md:w-24">
         <CircularText className={"fill-dark animate-spin-slow dark:fill-light"} />
 
-        <Link href="mailto:awdallaabdallah@gmail.com"
+        <Link href="mailto:mahe.chen@mail.utoronto.ca"
           className="flex items-center justify-center 
           absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark text-light shadow-md border border-solid border-dark w-20 h-20 rounded-full font-semibold hover:bg-light hover:text-dark 
 

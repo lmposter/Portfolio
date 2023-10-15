115d32746df5801e533bab5fdacd727a21596ce1
lmposter <111488731+lmposter@users.noreply.github.com>
2023-10-15 02:37:42 -0400

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -63,9 +63,9 @@ const about = () => {
               </p>
             </div>
 
-            <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light xl-col-span-4 md:order-1 md:col-span-8
+            <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light dark:bg-dark dark:border-light xl:col-span-4 md:col-span-8
           ">
-              <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light" />
+              <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light md:order-1" />
               <Image src={profilePic} alt="Mahe Chen" className='w-full h-auto rounded-2xl' priority
                 sizes='(max-width: 768px) 100vw, 
               (max-width: 1200px) 50vw,

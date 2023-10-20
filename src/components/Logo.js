cde93d46237e5615a41dbc73cdd9ef0c600f7b7f
Mahe Chen <111488731+lmposter@users.noreply.github.com>
2023-10-20 13:38:36 -0400

    Update Logo.js

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -14,7 +14,7 @@ const Logo = () => {
         backgroundColor: ["#121212", "rgba(131,58,180,1)","rgba(253,29,29,1)","rgba(252,176,69,1)","rgba(131,58,180,1)", "#121212"],
         transition: { duration: 0.5, repeat: Infinity, repeatType: "reverse" },
        }}
-      >MC</MotionLink>
+      >MAHECHEN.COM</MotionLink>
     </div>
   );
 }

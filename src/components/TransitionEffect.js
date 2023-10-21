94f3e8a1c500da3a1a5246488e699eb4a4683e1d
lmposter <111488731+lmposter@users.noreply.github.com>
2023-10-21 18:39:09 -0400

    Update TransitionEffect.js

--- src/components/TransitionEffect.js
+++ src/components/TransitionEffect.js
@@ -4,19 +4,19 @@ import { motion, AnimatePresence } from "framer-motion";
 const TransitionEffect = () => {
   return (
     <>
-      <motion.div className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-blue-950"
+      <motion.div className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-blue-500"
         initial={{ x: "100%", w: "100%" }}
         animate={{ x: "0%", w: "0%" }}
         exit={{x:["0%","100%"], width:["0%","100%"]}}
         transition={{ duration: 0.8, ease: "easeInOut" }}
       />
 
-      <motion.div className="fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-blue-900"
+      <motion.div className="fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-white"
         initial={{ x: "100%", w: "100%" }}
         animate={{ x: "0%", w: "0%" }}
         transition={{ delay:0.2, duration: 0.8, ease: "easeInOut" }}
       />
-      <motion.div className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-blue-800"
+      <motion.div className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-red-500"
         initial={{ x: "100%", w: "100%" }}
         animate={{ x: "0%", w: "0%" }}
         transition={{ delay:0.4, duration: 0.8, ease: "easeInOut" }}
